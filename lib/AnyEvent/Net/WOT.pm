d33774bd2f006cde22dbb1d493e8ab953eaa6fb3
Nikolay Shulyakovskiy <n.shulyakovskiy@corp.mail.ru>
2014-07-12 05:31:56 +0400

    Any fixes

--- lib/AnyEvent/Net/WOT.pm
+++ lib/AnyEvent/Net/WOT.pm
@@ -3,7 +3,8 @@ package AnyEvent::Net::WOT;
 use strict;
 use utf8;
 use AnyEvent::HTTP;
-
+use Mouse;
+use AnyEvent::Net::WOT::Storage;
 our $VERSION = '1.01';
 
 =head1 NAME
@@ -45,24 +46,24 @@ has server           => ( is => 'rw', isa => 'Str',     required => 1 );
 has version          => ( is => 'rw', isa => 'Str',     default => '0.4' );
 has api_method       => ( is => 'rw', isa => 'Str',     default => 'public_link_json2');
 has user_agent       => ( is => 'rw', isa => 'Str',     default => 'AnyEvent::Net::WOT client '.$VERSION );
-has http_timeout     => ( is => 'rw', isa => 'Numeric', default => 1 );
+has http_timeout     => ( is => 'rw', isa => 'Num',     default => 1 );
 has storage          => ( is => 'rw', isa => 'Object',  default => sub {AnyEvent::Net::WOT::Storage->new()});
-has cache_bad_answer => ( is => 'rw', isa => 'Boolean', default => 0);
-has default_answer   => ( is => 'rw', isa => 'HashRef', default => {0 => [50, 50], 4 => [50, 50]});
+has cache_bad_answer => ( is => 'rw', isa => 'Bool',    default => 0);
+has default_answer   => ( is => 'rw', isa => 'HashRef', default => sub{ return {0 => [50, 50], 4 => [50, 50]}});
 
 
-has caching_time => ( is => 'rw', isa => 'Numeric', default => 60*60*24 );
-has bad_caching_time => ( is => 'rw', isa => 'Numeric', default => 60*30);
+has caching_time => ( is => 'rw', isa => 'Num', default => 60*60*24 );
+has bad_caching_time => ( is => 'rw', isa => 'Num', default => 60*30);
 
 
 sub lookup {
 	my ($self, $hosts, $cb_ret) = @_;
-	$hosts = [$hosts] unless ref $host eq 'ARRAY';
+	$hosts = [$hosts] unless ref $hosts eq 'ARRAY';
 	die "Required callback" unless $cb_ret;
 	die "Must include at most 100 target names" if @$hosts > 100;
 	foreach( @$hosts ){
 		next unless m{/};
-		my $uri = URI->new($url)->canonical;
+		my $uri = URI->new($_)->canonical;
 		die "Bad url ".$_ if $uri->scheme !~ /^https?$/;
 		$_ = $uri->host;
 	}
@@ -76,7 +77,7 @@ sub lookup {
 		}
 		$url .= '&key='.$self->key;
 		die "the full request path must also be less than 8 KiB or it will be rejected" if length($url) > 8192; #ToDo paralel request
-		http_get $usr, %{$self->param_for_http_req}, sub {
+		http_get $url, %{$self->param_for_http_req}, sub {
 			my ($header, $body) = @_;
 			my $result = {};
 			eval{ $result = JSON::XS->decode_json($body) } if $header->{status} == 200 && $body;
