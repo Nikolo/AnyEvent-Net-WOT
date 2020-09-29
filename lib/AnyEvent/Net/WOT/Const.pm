fb66c448239b8a4ebda92c17030c5f06894f5828
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-09-29 13:38:23 +0300

    MMSYS-9810 add ae net wot const module

---
+++ lib/AnyEvent/Net/WOT/Const.pm
@@ -0,0 +1,10 @@
+package AnyEvent::Net::WOT::Const;
+
+use constant {
+    STATUS_SAFE       => 'SAFE',
+    STATUS_NOT_SAFE   => 'NOT_SAFE',
+    STATUS_UNKNOWN    => 'UNKNOWN',
+    STATUS_SUSPICIOUS => 'SUSPICIOUS',
+};
+
+1;
